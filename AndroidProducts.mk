4577e084f1328ed8afbbbd783617ca2054ec76b5
deelvoxed <delvoxed729@gmail.com>
2021-11-03 02:04:41 +0000

    cleanup bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_ginkgo.mk
+    $(LOCAL_DIR)/havoc_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_ginkgo-user \
-    arrow_ginkgo-userdebug \
-    arrow_ginkgo-eng
+    havoc_ginkgo-user \
+    havoc_ginkgo-userdebug \
+    havoc_ginkgo-eng
